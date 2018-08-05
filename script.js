299425bab86b11b1deb613cf4380d0d60ea58710
David Lovato <dalovato@ncsu.edu>
2018-08-05 16:55:13 -0400

    can determine if we have a winner, need to add AI

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
  * This file runs the script containg the game functionality and logic.
  * @author David Lovato
  */
-
 var origBoard;
 const huPlayer = 'O';
 const aiPlayer = 'X';
@@ -44,5 +43,23 @@ function turn(squareId, player) {
 }
 
 function checkWin(board, player) {
+    let plays = board.reduce((a, e, i) => (e === player) ? a.concat(i) : a, []);
+    let gameWon = null;
+    for (let [index, win] of winCombos.entries()) {
+        if (win.every(elem => plays.indexOf(elem) > -1)) {
+            gameWon = {index: index, player: player};
+            break;
+        }
+    }
+    return gameWon;
+}
 
+function gameOver(gameWon) {
+    for (let index of winCombos[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor =
+            gameWon.player == huPlayer ? "blue" : "red";
+    }
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener('click', turnClick, false);
+    }
 }
